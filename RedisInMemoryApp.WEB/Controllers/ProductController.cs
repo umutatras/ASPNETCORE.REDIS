c84ad400826a7c14fa56584334bb1933a8cde7b6
Umut Atraş <umutatras@outlook.com>
2024-07-17 14:15:26 +0300

    trygetvalue ve remove methodları denendi

--- RedisInMemoryApp.WEB/Controllers/ProductController.cs
+++ RedisInMemoryApp.WEB/Controllers/ProductController.cs
@@ -12,6 +12,34 @@ namespace RedisInMemoryApp.WEB.Controllers
 		}
 		public IActionResult Index()
 		{
+			//değerin bellekte olup olmadığını kontrol etme
+			if(String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+			{
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+
+            }
+			if(!_memoryCache.TryGetValue("zaman",out  string zaman))
+			{
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+
+            }
+            //değeri set etme key value şeklindedir.
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+			return View();
+		}
+
+		public IActionResult Show()
+		{
+			//veriyi önce dbden almaya çalışır yoksa set eder
+			_memoryCache.GetOrCreate<string>("zaman", entry =>
+			{
+				return DateTime.Now.ToString();
+			});
+
+			//bellekteki veriyi silmeyi sağlar
+			_memoryCache.Remove("zaman");
+			//veriyi çekme gösterme
+			ViewBag.zaman=_memoryCache.Get<string>("zaman");
 			return View();
 		}
 	}
