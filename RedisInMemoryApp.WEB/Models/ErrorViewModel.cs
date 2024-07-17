bd0cba22eb13255254b8d5c59e0c94ff79c32ce3
Umut Atraş <umutatras@outlook.com>
2024-07-17 12:08:47 +0300

    Add project files.

---
+++ RedisInMemoryApp.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RedisInMemoryApp.WEB.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
