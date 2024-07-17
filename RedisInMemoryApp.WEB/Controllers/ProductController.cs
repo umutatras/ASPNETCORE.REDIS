b71af5f75a0217fd4d0bb15016ec11f1aeebdf72
Umut Atraş <umutatras@outlook.com>
2024-07-17 13:37:49 +0300

    memorycache ctor yapıldı ve programcs eklendi

---
+++ RedisInMemoryApp.WEB/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace RedisInMemoryApp.WEB.Controllers
+{
+	public class ProductController : Controller
+	{
+		private  IMemoryCache _memoryCache;
+		public ProductController(IMemoryCache memoryCache)
+		{
+			_memoryCache = memoryCache;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
