711b65532c3ddcc42b6495bb7d5f65c040196cfc
Umut Atraş <umutatras@outlook.com>
2024-07-19 18:05:36 +0300

    remove ve get methodu kullanımı

--- IDistributedCacheRedisApp.WEB/Controllers/Productscontroller.cs
+++ IDistributedCacheRedisApp.WEB/Controllers/Productscontroller.cs
@@ -12,11 +12,27 @@ namespace IDistributedCacheRedisApp.WEB.Controllers
             _cache = cache;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             DistributedCacheEntryOptions options = new DistributedCacheEntryOptions();
             options.AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(1);
             _cache.SetString("name", "UMUT", options);
+
+            await _cache.SetStringAsync("surname", "Atras", options);
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            var name=_cache.GetString("name");
+            ViewBag.name = name;
+            return View();
+
+        }
+
+        public IActionResult Remove()
+        {
+            _cache.Remove("name");
             return View();
         }
     }
