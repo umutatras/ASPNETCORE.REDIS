58d2f2f4ae3c18c37c8b1fb4eaf34ecf16bb983e
Umut Atraş <umutatras@outlook.com>
2024-07-22 17:22:50 +0300

    sortedset kullanımı

---
+++ RedisExchangeAPI.WEB/Controllers/SortedTypeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.WEB.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.WEB.Controllers
+{
+    public class SortedTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private string listKey = "sortedsetnames";
+        public SortedTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+       
+        public IActionResult Index()
+        {
+            HashSet<string> keys = new HashSet<string>();
+
+            if(GetDb().KeyExists(listKey))
+            {
+                foreach (var item in GetDb().SortedSetScan(listKey).ToList())
+                {
+                    keys.Add(item.ToString());
+                };
+
+                foreach (var item in GetDb().SortedSetRangeByRank(listKey, order: Order.Ascending).ToList())
+                {
+                    keys.Add(item.ToString());
+                }
+            }
+            return View(keys);
+        }
+        public IActionResult DeleteItem(string name)
+        {
+            GetDb().SortedSetRemove(listKey, name);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Add(string name,int score)
+        {
+            GetDb().KeyExpire(listKey, DateTime.Now.AddMinutes(1));
+            GetDb().SortedSetAdd(listKey,name,score);
+            return RedirectToAction("Index");
+        }
+        private IDatabase GetDb()
+        {
+            return _redisService.GetDb(1);
+        }
+    }
+}
