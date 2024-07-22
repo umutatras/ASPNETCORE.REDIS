87b84aa2a40a84aa080e8f83e187320ae58cf2bc
Umut Atraş <umutatras@outlook.com>
2024-07-22 18:19:05 +0300

    Repo oluşturuldu

---
+++ RedisExample.API/Repository/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using RedisExample.API.Models;
+
+namespace RedisExample.API.Repository
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task<Product> CreateAsync(Product product);
+    }
+}
---
+++ RedisExample.API/Repository/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using RedisExample.API.Models;
+
+namespace RedisExample.API.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Product> CreateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
