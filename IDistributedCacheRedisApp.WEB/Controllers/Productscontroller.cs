fe372d908eea7579aa83080441d29c37fe2d13c5
Umut Atraş <umutatras@outlook.com>
2024-07-22 10:31:18 +0300

    Dosya kaydedilmesi yapıldı

--- IDistributedCacheRedisApp.WEB/Controllers/Productscontroller.cs
+++ IDistributedCacheRedisApp.WEB/Controllers/Productscontroller.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IDistributedCacheRedisApp.WEB.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
 
 namespace IDistributedCacheRedisApp.WEB.Controllers
 {
@@ -16,16 +20,31 @@ namespace IDistributedCacheRedisApp.WEB.Controllers
         {
             DistributedCacheEntryOptions options = new DistributedCacheEntryOptions();
             options.AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(1);
-            _cache.SetString("name", "UMUT", options);
+            Product product=new Product { Id = 1, Name="kalem",Price=100 };
+            string jsonProduct=JsonConvert.SerializeObject(product);
+            await _cache.SetStringAsync("product:1", jsonProduct,options);//complex type  kaydetmek istediğimiz verilerde seriliaze yaparız.
 
-            await _cache.SetStringAsync("surname", "Atras", options);
+            //binary olarak kaydetme
+            //Byte[] byteproduct=Encoding.UTF8.GetBytes(jsonProduct);
+            //_cache.Set("product:1", byteproduct);
+            //_cache.SetString("name", "UMUT", options);
+
+            //await _cache.SetStringAsync("surname", "Atras", options);
             return View();
         }
 
         public IActionResult Show()
         {
-            var name=_cache.GetString("name");
-            ViewBag.name = name;
+            //byte olarak okuma
+            //Byte[] byteproduct = _cache.Get("product:1");
+            //string jsonproduct=Encoding.UTF8.GetString(byteproduct);
+
+
+            //var name=_cache.GetString("name");
+            //ViewBag.name = name;
+            string jsonProduct = _cache.GetString("product:1");
+            Product p= JsonConvert.DeserializeObject<Product>(jsonProduct)!;
+            ViewBag.p= p;
             return View();
 
         }
@@ -33,6 +52,19 @@ namespace IDistributedCacheRedisApp.WEB.Controllers
         public IActionResult Remove()
         {
             _cache.Remove("name");
+            return View();
+        }
+        public IActionResult ImageUrl()
+        {
+            byte[] resimbyte = _cache.Get("resim");
+            return File(resimbyte,"image/jpg");
+        }
+        public IActionResult ImageCache()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/araba.jpg");
+            byte[] imageByte=System.IO.File.ReadAllBytes(path);
+            _cache.Set("resim", imageByte);
+            
             return View();
         }
     }
