1ec1b9218442d241c0988ee7229d5690a0984968
Umut Atraş <umutatras@outlook.com>
2024-07-22 14:39:43 +0300

    Redisset kullanımı(hashset muadili)

---
+++ RedisExchangeAPI.WEB/Controllers/SetTypeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.WEB.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.WEB.Controllers
+{
+    public class SetTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private string listKey = "hashnames";
+
+        public SetTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            if(!GetDb().KeyExists(listKey))
+            {
+
+            GetDb().KeyExpire(listKey,DateTime.Now.AddMinutes(5));
+     
+            }
+            GetDb().SetAdd(listKey, name);
+            return RedirectToAction("Index");
+        }
+        private IDatabase GetDb()
+        {
+            return _redisService.GetDb(2);
+        }
+    }
+}
