052c36ccb13803ebdb0db9b808c1633386998e25
Umut Atraş <umutatras@outlook.com>
2024-07-23 09:17:37 +0300

    programcs yapılandırması

--- RedisExample.API/Program.cs
+++ RedisExample.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RedisExample.API.Models;
+using RedisExample.API.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseInMemoryDatabase("mydatabase");
 });
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
